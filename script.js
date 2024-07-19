21c49de6c3f3bf747cd4d14c697bed59378d74a1
Chikwado626 <chikwadoezemma03@gmail.com>
2024-07-19 20:54:56 +0100

    script.js

--- script.js
+++ script.js
@@ -1,3 +1,55 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Your code to be executed after the DOM is loaded
-  });
+
+document.addEventListener('DOMContentLoaded', () => {
+    (function() {
+        // Form selection
+        const form = document.getElementById('registration-form');
+        form.addEventListener('submit', (event) => {
+            event.preventDefault();
+
+            // Retrieve user inputs and trim
+            const usernameInput = document.getElementById('username');
+            const username = usernameInput.value.trim();
+
+            const emailInput = document.getElementById('email');
+            const email = emailInput.value.trim();
+
+            const passwordInput = document.getElementById('password');
+            const password = passwordInput.value.trim();
+
+            // Initialize validation variables
+            let isValid = true;
+            let messages = [];
+
+            if (username.length < 3) { // username validation
+                isValid = false;
+                messages.push("Your username is less than 3 characters");
+            }
+
+            if (!email.includes('.') || !email.includes('.com')) { // email validation
+                isValid = false;
+                messages.push("Enter a valid email");
+            }
+
+            if (password.length < 7) { // password validation
+                isValid = false;
+                messages.push("Password must be 7 characters or more");
+            }
+
+            // Feedback div selection
+            const feedbackDiv = document.getElementById('form-feedback');
+
+            // feedback display logic
+            feedbackDiv.style.display = "block";
+            if (isValid) {
+                feedbackDiv.textContent = "Registration successful";
+                feedbackDiv.style.color = "#28a745";
+                feedbackDiv.style.backgroundColor = "#baffd4";
+            } else {
+                feedbackDiv.innerHTML = messages.join("<br>");
+                feedbackDiv.style.color = "#dc3545";
+                feedbackDiv.style.backgroundColor = "#ffbaba";
+
+            }
+        });
+    })();
+});
